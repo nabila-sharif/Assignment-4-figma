2f56f1e781d35225caa7a4c15dc9b9a4fdbb6f4b
nabila sharif <161031798+nabila-sharif@users.noreply.github.com>
2024-11-10 22:25:59 +0500

    figma

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+interface HeaderProps{
+  font:{
+    className: string;
+  };
+}
+export default function Header({font}:HeaderProps) {
+    return (
+      //sementic HTML
+      <>
+      <header className={`${font.className} links-parent`}>
+         <ul className="links"> 
+          <li className="link"> <Link href="">Works</Link></li>
+          <li className="link" > <Link href="">Blog</Link></li>
+          <li className="link" > <Link href="">Contact</Link></li>
+         </ul>
+      </header>
+      </>
+  );
+  }
